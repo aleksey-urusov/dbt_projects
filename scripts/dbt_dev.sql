c3ea6fcbc80e4adf5df1097fbb98e58e0c858c37
aleksey-urusov <au_web@pm.me>
2025-12-04 23:14:39 +0000

    created tables

---
+++ scripts/dbt_dev.sql
@@ -0,0 +1,6 @@
+use dbt;
+
+create or replace table dbt_projects.customer (customer_id int primary key, customer_name varchar);
+create table dbt_projects.bank_account 
+(accound_id int,account_num varchar,routing_num varchar,customer_id int,CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES dbt.dbt_projects.customer(customer_id)
+);
